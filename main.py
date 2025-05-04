0a43dcbb848ab190fb91806d7edd798709cd7ce6
Channary <channary@Channarys-Laptop.local>
2025-05-04 23:06:14 +0700

    add model translate en to fr

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class TextRequest(BaseModel):
 device = torch.device("cpu")
 
 # Load the GPT-2 text generation model
-text_generator = pipeline("text2text-generation", model="google/flan-t5-small",device=-1)
+translator = pipeline("translation_en_to_fr", model="Helsinki-NLP/opus-mt-en-fr", device=-1)
 
 @app.post("/sentiment_analysis")
 async def sentiment_analysis(request: TextRequest):
@@ -30,7 +30,7 @@ async def sentiment_analysis(request: TextRequest):
 class QARequest(BaseModel):
     question: str
 
-@app.post("/generate_answer")
+@app.post("/translation_en_to_fr")
 async def generate_answer(request: QARequest):
-    result = text_generator(request.question, max_length=50, truncation=True)[0]
-    return {"answer": result['generated_text']}
+    result = translator(request.question, max_length=50, truncation=True)[0]
+    return {"answer": result['translation_text']}
